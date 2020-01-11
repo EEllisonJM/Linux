db0fb08e55d9dc12953cf6fd7908a4091b4cdbab
EEllisonJM <ellison_jm@hotmail.com>
2020-01-11 13:01:13 -0600

    Set time for dual boot - windows - linux

---
+++ Ubuntu/time_dual_boot_off.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo "= = = = = Set Time linux - Dual boot Windows  - OFF= = = = ="
+timedatectl set-local-rtc 0 --adjust-system-clock
+timedatectl
---
+++ Ubuntu/time_dual_boot_on.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+echo "= = = = = Set Time linux - Dual boot Windows  - ON = = = = ="
+timedatectl set-local-rtc 1 --adjust-system-clock
+timedatectl
